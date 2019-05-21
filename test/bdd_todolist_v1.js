88f2b8b12715417419571e4d8cc3c05dd052b4df
Tomas Vosicky <vosicky@me.com>
2019-05-21 14:26:38 +0200

    .

--- test/bdd_todolist_v1.js
+++ test/bdd_todolist_v1.js
@@ -1,3 +1,4 @@
+/// <reference types="chai" />
 process.env.NODE_ENV = 'test';
 
 const chai = require('chai');
@@ -19,19 +20,12 @@ chai.use(chaiHttp);
 
 function dropCollections() {
   try {
-    User.collection.drop().then(result => {}).catch(err => {});
+    Promise.resolve()
+      .then(() => TodoList.remove({}))
+      .then(() => User.remove({}))
+      .then(() => TodoList.remove({}))
   } catch (error) {
-	   console.warn('Users collection may not exists!');
-  }
-  try {
-    TodoList.collection.drop().then(result => {}).catch(err => {});
-  } catch (error) {
-	   console.warn('TodoLists collection may not exists!');
-  }
-  try {
-    TodoListTask.collection.drop().then(result => {}).catch(err => {});
-  } catch (error) {
-     console.warn('TodoListTasks collection may not exists!');
+	   console.warn('Error!', error);
   }
 }
 
@@ -69,13 +63,31 @@ describe("TodoLists", () => {
   //
   describe('GET /api/v1/todolists', () => {
 	  it('it should receive a token error', (done) => {
-      // INSERT SOURCE CODE
-      done();
+      chai.request(server)
+        .post('/api/v1/todolists')
+        .send()
+        .end((err, res) => {
+          res.should.have.status(401);
+          res.body.should.be.deep.equal({
+            status: 401,
+            message: 'Unauthorized',
+            statusMessage: 'error',
+            errors: { Token: 'No authorization token was found.' }
+          });
+          done();
+        });
 	  });
 
     it('it should receive a list of todo lists', (done) => {
-      // INSERT SOURCE CODE
-      done();
+      chai.request(server)
+        .get('/api/v1/todolists')
+        // .auth(`Token ${apiToken}`, { options: { type: 'token' }})
+        .set('Authorization', `Token ${apiToken}`)
+        .send()
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
 	  });
 
     it('it should receive a list of todo lists (5 items)', (done) => {
