c1e31b804e6f85bd55a1610a033f3ec48e481fe3
Tomas Stary <tomas.stary@icloud.com>
2019-05-23 15:44:56 +0200

    Deleted version of node in travis config file

--- test/bdd_todolisttasks_v1.js
+++ test/bdd_todolisttasks_v1.js
@@ -26,17 +26,17 @@ function dropCollections() {
   try {
     User.collection.drop().then(result => {}).catch(err => {});
   } catch (error) {
-	   console.warn('Users collection may not exists!');
+    console.warn('Users collection may not exists!');
   }
   try {
     TodoList.collection.drop().then(result => {}).catch(err => {});
   } catch (error) {
-	   console.warn('TodoLists collection may not exists!');
+    console.warn('TodoLists collection may not exists!');
   }
   try {
     TodoListTask.collection.drop().then(result => {}).catch(err => {});
   } catch (error) {
-     console.warn('TodoListTasks collection may not exists!');
+    console.warn('TodoListTasks collection may not exists!');
   }
 }
 
@@ -250,18 +250,41 @@ describe('TodoListTasks', () => {
     };
 
     before((done) => {
-      // INSERT SOURCE CODE
-      done();
+      let todoListModel = todoListParams.todoList;
+      todoListModel.author = userId;
+      TodoList.create(todoListModel)
+        .then((todo) => {
+          todoListLocalId = todo._id;
+          let todoTaskModel = todoListTaskParams.task;
+          todoTaskModel.todoList = todo;
+          TodoListTask.create(todoTaskModel)
+            .then((task) => {
+              todoListTaskLocalId = task._id;
+              done();
+            });
+        });
     });
 
     it('it should receive a token error', (done) => {
-      // INSERT SOURCE CODE
-      done();
+      chai.request(server)
+        .get(`/api/v1/todolists/${todoListLocalId}/tasks/${todoListTaskLocalId}`)
+        .send()
+        .end((err, res) => {
+          res.should.have.status(401);
+          res.body.should.be.deep.equal(tokenError);
+          done();
+        });
     });
 
     it('it should receive an error 404 (Not found)', (done) => {
-      // INSERT SOURCE CODE
-      done();
+      chai.request(server)
+        .get(`/api/v1/todolists/${todoListLocalId}/tasks/123`)
+        .set('Authorization', `Token ${apiToken}`)
+        .send()
+        .end((err, res) => {
+          res.should.have.status(404);
+          done();
+        });
     });
 
     it('it should receive a todo list task', (done) => {
